3488ddd58e9e584098d10518bf3db588fe983fed
Dtaa <dtaaa001@gmail.com>
2024-02-13 23:30:06 +0700

    Update terraform variables.tf file

--- week-1-docker-terraform/terraform/main.tf
+++ week-1-docker-terraform/terraform/main.tf
@@ -1,33 +1,33 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_s3_bucket" "terraform_demo" {
-  bucket = "dtaa-de-tf"
+  bucket        = var.aws_s3_bucket_name
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-    bucket = aws_s3_bucket.terraform_demo.id
-    versioning_configuration {
-      status = "Enabled"
-    }
+  bucket = aws_s3_bucket.terraform_demo.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "terraform_bucket_lifecycle_rule" {
   bucket = aws_s3_bucket.terraform_demo.bucket
 
   rule {
-    id = "example-rule"
+    id     = "example-rule"
     status = "Enabled"
 
     abort_incomplete_multipart_upload {
@@ -36,3 +36,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform_bucket_lifecycle_rul
   }
 }
 
+resource "aws_redshift_cluster" "tf_redshift_demo" {
+  cluster_identifier        = "tf-redshift-cluster"
+  database_name             = "mydb"
+  master_username           = "dtaa"
+  master_password           = "Dtaa0673."
+  node_type                 = "ra3.xlplus"
+  cluster_type              = "single-node"
+  final_snapshot_identifier = "my-final-snapshot"
+}
---
+++ week-1-docker-terraform/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "Region"
+  default     = "us-east-1"
+}
+
+variable "aws_s3_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "dtaa-de-tf"
+}
+
